f67df66d2c5f2fbff82f7018605729ae89fa6571
1850094299@qq.com <1850094299@qq.com>
2020-06-14 20:58:30 +0800

    pandas

--- pandasStu/DataFrameStu.py
+++ pandasStu/DataFrameStu.py
@@ -7,3 +7,20 @@ import numpy as np
 
 d1 = pd.DataFrame(np.arange(12).reshape(3, 4))
 print(d1)
+p1 = pd.DataFrame(np.arange(12).reshape(3, 4), index=list("abc"), columns=list("WXYZ"))
+print(p1)
+p1 = pd.DataFrame(np.arange(12).reshape(3, 4), index=list("abc"), columns=list("WXYZ"))
+print(p1)
+print("*" * 100)
+d1 = {"name": ["xiaoming", "xiaogang"], "age": [20, 32], "tel": [10086, 10010]}
+t1 = pd.DataFrame(d1)
+print("*" * 100)
+print(type(t1))
+d2 = [
+    {"name": "xiaohong", "age": 32, "tel": 10010},
+    {"name": "xiaogang", "tel": 10000},
+    {"name": "xiaowang", "age": 32}
+]
+t2=pd.DataFrame(d2)
+print(t2)
+
