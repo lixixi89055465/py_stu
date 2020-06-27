4f097df81086511328075c9acf6d23702d9d72a7
1850094299@qq.com <1850094299@qq.com>
2020-06-27 22:05:03 +0800

    numpy

---
+++ tangyudi/base/numpy/example02.py
@@ -0,0 +1,16 @@
+# encoding: utf-8
+"""
+@author: nanjixiong
+@time: 2020/6/27 22:01
+@file: example02.py
+@desc: 
+"""
+import numpy as np
+vector=np.array([5,10,15,20])
+matrix=np.array([[5,10,15],[20,25,30],[35,40,45]])
+print(vector)
+print(matrix)
+vector=np.array([1,2,3,4])
+print(vector.shape)
+matrix=np.array([[5,10,15],[20,25,30]])
+print(matrix.shape)
