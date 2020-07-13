fdc7aeda772407eb8f2c2f87923dd1715c62a3ae
1850094299@qq.com <1850094299@qq.com>
2020-07-13 22:50:25 +0800

    logistic regression

--- tangyudi/配套案例/梯度下降/test3.py
+++ tangyudi/配套案例/梯度下降/test3.py
@@ -17,6 +17,7 @@ fig,ax=plt.subplots(figsize=(10,5))
 ax.scatter(positive['Exam 1'],positive['Exam 2'],s=50,c='b',marker='o',label='Admitted')
 ax.scatter(negative['Exam 1'],negative['Exam 2'],s=30,c='r',marker='x',label='Not Admitted')
 
-# ax.legend()
-# ax.set_xlabel('Exam 1 Score')
-# ax.set_ylabel('Exam 2 Score')
+
+ax.legend()
+ax.set_xlabel('Exam 1 Score')
+ax.set_ylabel('Exam 2 Score')
