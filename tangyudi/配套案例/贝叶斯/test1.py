3408646f2cac712a966480c5c480e88233bc166f
nanji <1850094299@qq.com>
2020-10-03 15:06:59 +0800

    贝叶斯：init

--- tangyudi/配套案例/贝叶斯/test1.py
+++ tangyudi/配套案例/贝叶斯/test1.py
@@ -1,2 +1,19 @@
-import re
-print(re.findall('[a-z]+', 'Tian Kong zhi cheng '.lower()))
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+
+def edits1(word):
+    n = len(word)
+    return set([word[0:i] + word[i + 1:] for i in range(n)] +  # deletion
+               [word[0:i] + word[i + 1] + word[i] + word[i + 2:] for i in range(n - 1)] +  # transposition
+               [word[0:i] + c + word[i + 1:] for i in range(n) for c in alphabet] +  # alteration
+               [word[0:i] + c + word[i:] for i in range(n + 1) for c in alphabet])  # insertion
+
+
+word = 'xiaoming'
+n = len(word)
+
+# print(set(word[0:i]+word[i+1]+word[i]+word[i+2:] for i in range(n-1)))
+
+# print(list(word[0:i]+c+word[i+1:] for i in range(n) for c in alphabet))
+
+print(list(word[0:i] + c + word[i:] for i in range(n + 1) for c in alphabet))
