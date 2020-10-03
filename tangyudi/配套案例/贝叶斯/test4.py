94d1df089f260e466fecda2a3fc8386b2e452cf5
nanji <1850094299@qq.com>
2020-10-03 22:52:31 +0800

    贝叶斯：init

--- tangyudi/配套案例/贝叶斯/test4.py
+++ tangyudi/配套案例/贝叶斯/test4.py
@@ -1,4 +1,5 @@
 import re, collections
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
 
 def train(features):
@@ -11,8 +12,38 @@ def train(features):
 def words(text): return re.findall('[a-z]+', text.lower())
 
 
-print(words("tianKongZHIJDS"))
-features = ["tian", "kong", "zhi", "cheng", "tian"]
+def edits1(word):
+    n = len(word)
+    return set([word[0:i] + word[i + 1:] for i in range(n)] +  # deletion
+               [word[0:i] + word[i + 1] + word[i] + word[i + 2:] for i in range(n - 1)] +  # transposition
+               [word[0:i] + c + word[i + 1:] for i in range(n) for c in alphabet] +  # alteration
+               [word[0:i] + c + word[i:] for i in range(n + 1) for c in alphabet])  # insertion
 
-model = train(features)
-print(model)
+
+def known_edits2(word):
+    return set(e2 for e1 in edits1(word) for e2 in edits1(e1) if e2 in NWORDS)
+
+
+def known(words): return set(w for w in words if w in NWORDS)
+
+
+def correct(word):
+    candidates = known([word]) or known(edits1(word)) or known_edits2(word) or [word]
+    return max(candidates, key=lambda w: NWORDS[w])
+
+
+# print(words("tianKongZHIJDS"))
+# features = ["tian", "kong", "zhi", "cheng", "tian"]
+
+# model = train(features)
+# print(model)
+
+# print('1'*100)
+# NWORDS = train(words(open('big.txt').read()))
+# print(NWORDS)
+
+
+NWORDS = train(words(open('big.txt').read()))
+print(NWORDS.__sizeof__())
+print(NWORDS.items())
+print(NWORDS)
