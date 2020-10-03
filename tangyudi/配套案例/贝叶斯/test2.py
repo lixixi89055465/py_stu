2158222ef926e49901c38ae4b82649d547537ec1
nanji <1850094299@qq.com>
2020-10-03 22:21:43 +0800

    贝叶斯：init

--- tangyudi/配套案例/贝叶斯/test2.py
+++ tangyudi/配套案例/贝叶斯/test2.py
@@ -1,6 +1,14 @@
-import re
+import re, collections
 
-def word(text):
-    return re.findall('[a-z]+',text.lower())
 
-word("afsf")
+def words(text): return re.findall('[a-z]+', text.lower())
+
+
+def train(features):
+    model = collections.defaultdict(lambda: 1)
+    for f in features:
+        model[f] += 1
+    return model
+
+
+print(collections.defaultdict(lambda: 1))
--- tangyudi/配套案例/贝叶斯/test3.py
+++ tangyudi/配套案例/贝叶斯/test3.py
@@ -1,2 +1,22 @@
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-print([c+str(i) for i in range(5) for c in alphabet])
+# %
+# from collections import defaultdict
+# s=[('yellow',1),('blue', 2), ('yellow', 3), ('blue', 4), ('red', 1)]
+# d=defaultdict(list)
+# for k, v in s:
+#     d[k].append(v)
+# a=sorted(d.items())
+# print(a)
+
+from collections import defaultdict
+
+s = [('yellow', 1), ('blue', 2), ('yello', 3), ('blue', 4), ('red', 1),('yello',333)]
+print(type(s[0]))
+print(type(s))
+d = defaultdict(list)
+print(type(d))
+
+for k, v in s:
+    d[k].append(v)
+s = sorted(d.items())
+print(s)
+print(d)
