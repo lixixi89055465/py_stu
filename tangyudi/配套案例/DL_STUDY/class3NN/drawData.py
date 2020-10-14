14d50df58efedf250c9e163f658902683742a675
nanji <1850094299@qq.com>
2020-10-14 20:37:21 +0800

    神经网络

--- tangyudi/配套案例/DL_STUDY/class3NN/drawData.py
+++ tangyudi/配套案例/DL_STUDY/class3NN/drawData.py
@@ -10,17 +10,30 @@ np.random.seed(10)
 N = 100  # number of points per class
 D = 2  # dimensionality
 K = 3
-X = np.zeros((N * K), D)
+X = np.zeros((N * K, D))
 
 y = np.zeros(N * K, dtype='uint8')
 
+# for j in range(K):
+#     ix = range(N * j, N * (j + 1))
+#     r = np.linspace(0.0, 1, N)
+#     # t = np.linspace(j * 4, (j + 1) * 4, N) + np.random.randn(N) * 0.2  # theta
+#     t = np.linspace(j * 4, (j + 1) * 4, N)
+#     X[ix] = np.c_[r * np.sin(t)*2, r * np.cos(t)*2]
+#     y[ix] = j
+#
+
+
 for j in range(K):
-    ix = range(N * j, N * (j + 1))
-    r = np.linspace(0.0, 1, N)
-    t = np.linspace(j * 4, (j + 1) * 4, N) + np.random.randn(N) * 0.2  # theta
-    X[ix] = np.c_[r * np.sin(t), r * np.cos(t)]
-    y[ix] = j
+    ix=range(N*j,N*(j+1))
+    r=np.linspace(0.0,1,N)
+    t=np.linspace(j*4,(j+1)*4,N)+np.random.randn(N)*0.2
+    X[ix]=np.c_[r*np.sin(t)*2,r*np.cos(t)*2]
+    y[ix]=j
 
+fig = plt.figure()
 
-W = 0.01 * np.random.randn(D, K)
-b = np.zeros((1, K))
+plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.Spectral)
+plt.xlim([-1, 1])
+plt.ylim([-1, 1])
+plt.show()
