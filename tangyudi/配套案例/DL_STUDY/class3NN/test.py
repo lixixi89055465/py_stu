b02c3e3aa0d7ca9bdab5c18b4373ca22049d62d6
nanji <1850094299@qq.com>
2020-10-14 20:50:33 +0800

    神经网络

--- tangyudi/配套案例/DL_STUDY/class3NN/test.py
+++ tangyudi/配套案例/DL_STUDY/class3NN/test.py
@@ -3,7 +3,9 @@ import numpy as np
 # print(np.random.randn(10))
 # print(np.linspace(10, 20, 10,endpoint=False))
 
-ax=range(4,5)
-print(ax)
-for i in range(4,15):
-    print(i)
+# ax=range(4,5)
+# print(ax)
+# for i in range(4,15):
+#     print(i)
+
+np.arange()
