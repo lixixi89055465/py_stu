2a0968d63d2d02577e906117ec40aa3f9c001416
lixiang <1850094299@qq.com>
2020-12-20 00:56:53 +0800

    模拟普通的数据训练

---
+++ muli/base/chapter1/utils.py
@@ -0,0 +1,17 @@
+from mxnet import ndarray as nd
+from mxnet import  gluon
+import mxnet as mx
+
+def SGD(params,lr):
+    for param in params:
+        param[:]=param-lr*param.grad
+def accuracy(output,label):
+    return nd.mean(output.argmax(axis=1)==label).asscalar()
+
+
+def evaluate_accuracy(data_iterator,net):
+    acc=0
+    for data,label in data_iterator:
+        output=net(data)
+        acc+=accuracy(output,label)
+    return acc / len(data_iterator)
