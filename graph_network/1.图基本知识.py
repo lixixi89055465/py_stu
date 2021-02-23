49f997a8498bd0580ad1631daf5d38a944d8c002
lixiang <1850094299@qq.com>
2021-02-23 17:41:35 +0800

    图神经网络

---
+++ graph_network/1.图基本知识.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import networkx as nx
+
+edges = pd.DataFrame()
+
+edges['sources'] = [1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 5]
+edges['targets'] = [2, 4, 5, 3, 1, 2, 5, 1, 5, 1, 3, 4]
+edges['weights'] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+
+G = nx.from_pandas_edgelist(edges, source='sources', target='targets',
+                            edge_attr='weights')
+# degree
+print(nx.degree(G))
+
+# 联通分量
+print(list(nx.connected_components(G)))
+# 图直径
+print(nx.diameter(G))
