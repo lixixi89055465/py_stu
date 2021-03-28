3a8b60dd512791b5a196b2a0665a626cf6706922
1850094299@qq.com <1850094299@qq.com>
2021-03-28 20:58:05 +0800

    'test'

--- a.sh
+++ a.sh
@@ -5,10 +5,10 @@ set timeout 30
 spawn git add * 
 spawn git commit -m 'test' 
 spawn git push origin master
-expect "Username for 'https://github.com'"
+expect "*Username*"
 send "1850094299@qq.com\r"
-expect "Password"
-send "89055465ab" 
+expect "*Password*"
+send "89055465ab\r" 
 
 
 
