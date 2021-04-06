0a0f68a477ce3050f9704e99e3bae94e9ef2139b
lixiang <1850094299@qq.com>
2021-04-06 21:50:22 +0800

    resnet 18 34 实现

---
+++ tensorflow_stu/ch10-卷积神经网络/Resnet18_34.py
@@ -0,0 +1,80 @@
+import tensorflow as tf
+import os
+from tensorflow.keras import datasets, layers, optimizers, Sequential, metrics, regularizers
+
+
+class BasicBlock(layers.Layer):
+    def __init__(self, filter_num, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = layers.Conv2D(filter_num, (3, 3), strides=stride, padding='same')
+        self.bn1 = layers.BatchNormalization()
+        self.relu = layers.Activation('relu')
+
+        self.conv2 = layers.Conv2D(filter_num, (3, 3), strides=1, padding='same')
+        self.bn2 = layers.BatchNormalization()
+
+        if stride != 1:
+            self.downsample = Sequential()
+            self.downsample.add(layers.Conv2D(filter_num, (1, 1), strides=stride))
+            self.downsample.add(layers.BatchNormalization())
+        else:
+            self.downsample = lambda x: x
+        self.stride = stride
+
+    def call(self, inputs, training=None):
+        conv1 = self.conv1(inputs)
+        bn1 = self.bn1(conv1)
+        relu1 = self.relu(bn1)
+
+        conv2 = self.conv2(relu1)
+        bn2 = self.bn2(conv2)
+
+        residual = self.downsample(inputs)
+        add = layers.add([bn2, residual])
+        # out = self.relu(add)
+        out = tf.nn.relu(add)
+        return out
+
+class Resnet(tf.keras.Model):
+    def __init__(self, layer_dims, num_classes=100):  # [2,2,2,2]
+        super(Resnet, self).__init__()
+        self.stem = Sequential([
+            layers.Conv2D(64, (3, 3), strides=(1, 1)),
+            layers.BatchNormalization(),
+            layers.Activation('relu'),
+            layers.MaxPool2D(pool_size=(2, 2), strides=(1, 1), padding='same')
+        ])
+        self.layer1 = self.build_resblock(64, layer_dims[0], stride=1)
+        self.layer2 = self.build_resblock(128, layer_dims[1], stride=2)
+        self.layer3 = self.build_resblock(256, layer_dims[2], stride=2)
+        self.layer4 = self.build_resblock(512, layer_dims[3], stride=2)
+
+        self.avgpool = layers.GlobalAveragePooling2D()
+        self.fc = layers.Dense(num_classes)
+
+    def call(self, inputs, training=None):
+        x = self.stem(inputs)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        # [b,c]
+        x = self.avgpool(x)
+        # [b,100]
+        x = self.fc(x)
+        return x
+
+    def build_resblock(self, filter_num, blocks, stride=1):
+        res_blocks = Sequential()
+        res_blocks.add(BasicBlock(filter_num, stride))
+        for _ in range(1, blocks):
+            res_blocks.add(BasicBlock(filter_num, stride=1))
+        return res_blocks
+
+
+def resnet18():
+    return Resnet([2, 2, 2, 2])
+
+
+def resnet34():
+    return Resnet([3, 4, 6, 3])
---
+++ tensorflow_stu/ch10-卷积神经网络/resnet34_train.py
@@ -0,0 +1,81 @@
+import tensorflow as tf
+import os
+
+from Resnet18_34 import resnet34
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+from tensorflow.keras import datasets, layers, optimizers, Sequential, metrics, regularizers
+
+gpu_options = tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=0.4)
+
+cpu_num = 16
+config = tf.compat.v1.ConfigProto(device_count={"CPU": cpu_num},
+                                  gpu_options=gpu_options,
+                                  inter_op_parallelism_threads=cpu_num,
+                                  intra_op_parallelism_threads=cpu_num,
+                                  log_device_placement=True)
+session = tf.compat.v1.Session(config=config)
+
+
+def preprocess(x, y):
+    x = 2 * tf.cast(x, dtype=tf.float32) / 255. - 1
+    y = tf.cast(y, dtype=tf.int32)
+    return x, y
+
+
+(x, y), (x_test, y_test) = datasets.cifar100.load_data()
+y = tf.squeeze(y, axis=1)
+y_test = tf.squeeze(y_test, axis=1)
+print(x.shape, y.shape, x_test.shape, y_test.shape)
+
+train_db = tf.data.Dataset.from_tensor_slices((x, y))
+train_db = train_db.shuffle(10000).map(preprocess).batch(128)
+test_db = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+test_db = test_db.map(preprocess).batch(128)
+
+
+def main():
+    model = resnet34()
+    model.build(input_shape=(None, 32, 32, 3))
+    model.summary()
+    optimizer = optimizers.Adam(lr=1e-4)
+
+    for epoch in range(200):
+        correct_sum = 0
+        correct_length = 0
+        for step, (x, y) in enumerate(train_db):
+            with tf.GradientTape() as tape:
+                logits = model(x)
+                pred = tf.nn.softmax(logits, axis=1)
+                pred = tf.argmax(pred, axis=1)
+                correct = tf.equal(pred, tf.cast(y, dtype=tf.int64))
+                correct_sum += tf.reduce_sum(tf.cast(correct, dtype=tf.int32))
+                correct_length += x.shape[0];
+                y_onehot = tf.one_hot(y, depth=100)
+                loss = tf.losses.categorical_crossentropy(y_onehot, logits, from_logits=True)
+                loss = tf.reduce_mean(loss)
+            grads = tape.gradient(loss, model.trainable_variables)
+            optimizer.apply_gradients(zip(grads, model.trainable_variables))
+            if step % 100 == 0:
+                print(epoch, step, 'loss:', float(loss))
+        print("test acc is ", float(correct_sum) / float(correct_length))
+        total_num = 0
+        total_correct = 0.0
+        for x, y in test_db:
+            logits = model(x)
+            prob = tf.nn.softmax(logits, axis=1)
+
+            pred = tf.argmax(prob, axis=1)
+            pred = tf.cast(pred, dtype=tf.int32)
+
+            correct = tf.cast(tf.equal(pred, y), dtype=tf.int32)
+            correct = tf.reduce_sum(correct)
+
+            total_num += x.shape[0]
+            total_correct += int(correct)
+
+        acc = total_correct / total_num
+        print(epoch, 'acc:', acc)
+
+
+main()
