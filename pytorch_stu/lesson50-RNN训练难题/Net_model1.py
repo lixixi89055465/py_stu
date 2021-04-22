82d9723cb35a5b6e6d64a3b212ec8d47d17e2c26
lixiang <1850094299@qq.com>
2021-04-22 18:54:59 +0800

    pytorch RNN-LSTM

--- pytorch_stu/lesson50-RNN训练难题/Net_model1.py
+++ pytorch_stu/lesson50-RNN训练难题/Net_model1.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+import matplotlib.pyplot as plt
 
 num_time_steps = 50
 input_size = 1
@@ -23,7 +24,7 @@ class Net(torch.nn.Module):
 
     def forward(self, x, hidden_prev):
         out, hidden_prev = self.rnn(x, hidden_prev)
-        out = out.view(-1, hidden_prev)
+        out = out.view(-1, hidden_size)
         out = self.linear(out)
         out = out.unsqueeze(dim=0)
         return out, hidden_prev
@@ -36,6 +37,41 @@ hidden_prev = torch.zeros(1, 1, hidden_size)
 
 for iter in range(6000):
     start = np.random.randint(3, size=1)[0]
-    break
+    time_steps = np.linspace(start, start + 10, num_time_steps)
+    data = np.sin(time_steps)
+    data = data.reshape(num_time_steps, 1)
+    x = torch.tensor(data[:-1]).float().view(1, num_time_steps - 1, 1)
+    y = torch.tensor(data[1:]).float().view(1, num_time_steps - 1, 1)
+    output, hidden_prev = model(x, hidden_prev)
+    hidden_prev = hidden_prev.detach()
+    loss = criterion(output, y)
+    model.zero_grad()
+    loss.backward()
+    for p in model.parameters():
+        torch.nn.utils.clip_grad_norm_(p, 10)
+    optimizer.step()
+    if iter % 100 == 0:
+        print("Iteration :{} loss {}".format(iter, loss.item()))
 
-print(start)
+start = np.random.randint(3, size=1)[0]
+time_steps = np.linspace(start, start + 10, num_time_steps)
+data = np.sin(time_steps)
+data = data.reshape(num_time_steps, 1)
+x = torch.tensor(data[:-1]).float().view(1, num_time_steps - 1, 1)
+y = torch.tensor(data[1:]).float().view(1, num_time_steps - 1, 1)
+
+predictions = []
+input = x[:, 0, :]
+for _ in range(x.shape[1]):
+    input = input.view(1, 1, 1)
+    (pred, hidden_prev) = model(input, hidden_prev)
+    input = pred
+    predictions.append(pred.detach().numpy().ravel()[0])
+
+x = x.data.numpy().ravel()
+y = y.data.numpy()
+plt.scatter(time_steps[:-1], x.ravel(), s=90)
+plt.plot(time_steps[:-1], x.ravel())
+
+plt.scatter(time_steps[1:], predictions)
+plt.show()
---
+++ pytorch_stu/lesson52-LSTM-Layer使用/main1.py
@@ -0,0 +1,8 @@
+import torch
+
+lstm = torch.nn.LSTM(input_size=100, hidden_size=20, num_layers=4)
+print(lstm)
+
+x = torch.randn(10, 3, 100)
+out, (h, c) = lstm(x)
+print(out.shape, h.shape, c.shape)
