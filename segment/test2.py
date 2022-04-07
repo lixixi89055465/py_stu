bb6983ce311a392050d7ef42adff4d8693d36735
nanji <1850094299@qq.com>
2022-04-07 14:55:41 +0800

    test

---
+++ segment/test2.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# a=np.mgrid[-4:4:2j]
+# print(a)
+a = np.mgrid[1:3:3j, 4:5:2j]
+print(a)
+# x, y = np.mgrid[1:3:3j, 4:5:2j]
+# print(x)
+# print(y)
+# import numpy as np
+#
+# b = np.mgrid[-1:1:2j, -2:2:2j, -3:3:5j]
+# print(b)
