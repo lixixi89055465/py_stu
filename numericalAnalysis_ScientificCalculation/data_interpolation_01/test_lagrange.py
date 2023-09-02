86f64ea2f5460a32389c9b893886b8d5628bfaea
lixixi89055465 <nanji1850094299@outlook.com>
2023-09-02 23:51:54 +0800

    auto commit:maching learn 2022

---
+++ numericalAnalysis_ScientificCalculation/data_interpolation_01/test_lagrange.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/9/2 22:17
+# @Author  : nanji
+# @Site    : 
+# @File    : test_lagrange.py
+# @Software: PyCharm 
+# @Comment :
+import numpy as np
+
+x = np.linspace(0, 24, 13, endpoint=True)
+y = np.array([12, 9, 9, 10, 18, 24, 28, 27, 25, 20, 18, 15, 13])
+
+print(x)
