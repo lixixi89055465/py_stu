750340341dd277bf16280c5c49e9bbc482ec2def
lixixi89055465 <nanji1850094299@outlook.com>
2023-09-03 16:36:26 +0800

    auto commit:maching learn 2022

---
+++ numericalAnalysis_ScientificCalculation/data_interpolation_01/test/test_aitken_stepwise_interp.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/9/2 22:17
+# @Author  : nanji
+# @Site    : 
+# @File    : test_lagrange.py
+# @Software: PyCharm 
+# @Comment :
+import numpy as np
+
+from numericalAnalysis_ScientificCalculation.data_interpolation_01.aitken_stepwise_interpolation import \
+    AitkenStepwiseInterpolation
+
+x = np.linspace(0, 2 * np.pi, 10, endpoint=True)
+y = np.sin(x)
+x0 = np.array([np.pi / 2, 2.158, 3.58, 4, 784])
+
+asi_interp = AitkenStepwiseInterpolation(x=x, y=y)
+asi_interp.fit_interp()
+print('艾特肯多项式如下:')
+print(asi_interp.polynomial)
+print('艾特肯插值多项式系数向量和对应阶次 :')
+print(asi_interp.poly_cofficient)
+print(asi_interp.coefficient_order)
+y0 = asi_interp.cal_interp_x0(x0)
+print('所求插值点的值:', y0, '插值点的精度:', np.sin(x0))
+print('0' * 100)
+asi_interp.plt_interpolation(x0)
