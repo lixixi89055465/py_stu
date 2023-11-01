6e794ca0aba6c0ba0eff9c08fc3417e6afa3b748
xiang.li4 <wb_xiang.li4@sinitek.com>
2023-11-01 19:55:58 +0800

    machine learning 2022

--- machinelearn/ml_cluster/LearningVectorQuantization.py
+++ machinelearn/ml_cluster/LearningVectorQuantization.py
@@ -5,10 +5,14 @@
 # @File    : LearningVectorQuantization.py
 # @Software: PyCharm 
 # @Comment :
+import copy
 import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import classification_report
 
 
-class LearningVectorQuantization:
+class LearningVectorQuantizationClustering:
     '''
     基于原型聚类：学习向量量化
     '''
@@ -53,3 +57,53 @@ class LearningVectorQuantization:
         '''
         # 初始化原型向量，随机选择
         random_idx = np.random.choice(self.m, len(self.class_label), replace=False)
+        for idx, r_idx in enumerate(random_idx):
+            self.cluster_centers_[idx] = X[r_idx]
+        for c, idx in enumerate(random_idx):
+            idx_s = list(range(self.m))
+            np.random.shuffle(idx_s)
+            cluster_center_old = copy.deepcopy(self.cluster_centers_)
+            eps = 0.
+            for idx in idx_s:  # 针对每一个随机样本
+                vec, yi = X[idx], y[idx]  # 取某一个样本和类别标记
+                min_dist, bst_cid = np.infty, None
+                for cid in self.cluster_centers_.keys():
+                    dist = self.distance_fun(vec, self.cluster_centers_[cid])
+                    if dist < min_dist:
+                        min_dist, bst_cid = dist, cid
+                # 更新原型向量
+                if yi == self.class_label[bst_cid]:
+                    self.cluster_centers_[bst_cid] = self.cluster_centers_[bst_cid] \
+                                                     + self.eta * (vec - self.cluster_centers_[bst_cid])
+                else:
+                    self.cluster_centers_[bst_cid] = self.cluster_centers_[bst_cid] \
+                                                     - self.eta * (vec - self.cluster_centers_[bst_cid])
+            # 终止条件
+            for key in self.cluster_centers_.keys():
+                eps += self.distance_fun(cluster_center_old[key] , self.cluster_centers_[key])
+            eps /= len(self.cluster_centers_)
+            if eps < self.tol:
+                break
+
+        print(random_idx)
+
+    def predict(self, X):
+        cluster_labels = []
+        for i in range(self.m):
+            best_k, min_dist = np.infty
+            for idx in range(len(self.cluster_centers_)):
+                dist = self.distance_fun(self.cluster_centers_[idx], X[i])
+                if dist < min_dist:
+                    best_k, min_dist = idx, dist
+            cluster_labels.append(best_k)
+        return cluster_labels
+
+
+if __name__ == '__main__':
+    data = pd.read_csv('../data/iris.csv')
+    X, y = data.iloc[:, :-1].values, data.iloc[:, -1]
+    y = LabelEncoder().fit_transform(y)
+    lvq = LearningVectorQuantizationClustering(X, y, eta=0.05, tol=1e-3)
+    lvq.fit_LVQ(X, y)
+    cluster_ind = lvq.predict(X)
+    print(classification_report(y, cluster_ind))
