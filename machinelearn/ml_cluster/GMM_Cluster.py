3d4e0570ce5d887d0f0427f3c5b6c343e3a1cce4
lixixi89055465 <1850094299@qq.com>
2023-11-04 00:00:46 +0800

    machine  learning 2022

--- machinelearn/ml_cluster/GMM_Cluster.py
+++ machinelearn/ml_cluster/GMM_Cluster.py
@@ -6,6 +6,7 @@
 # @Software: PyCharm 
 # @Comment :
 import numpy as np
+import pandas as pd
 
 
 class GaussianMixtureCluster:
@@ -33,7 +34,7 @@ class GaussianMixtureCluster:
         :param sigma: n*n
         :return:
         '''
-        left_part = 1.0 / (np.pow(2 * np.pi, X.shape[1] / 2) * np.sqrt(np.linalg.det(sigma)))
+        left_part = 1.0 / (np.power(2 * np.pi, X.shape[1] / 2) * np.sqrt(np.linalg.det(sigma)))
         right_part = np.exp(np.sum(-.5 * (X - mu).dot(np.linalg.inv(sigma)) * (X - mu), axis=1))
         return left_part * right_part
 
@@ -43,3 +44,30 @@ class GaussianMixtureCluster:
         :param X:
         :return:
         '''
+        alpha, mu, sigma = 1.0 / self.n_compoents, np.mean(X, axis=0), np.cov(X.T)
+        max_value, min_value = X.max(), X.min()
+        for i in range(self.n_compoents):
+            self.params.append([alpha, mu + np.random.random() * (max_value + min_value) / 2, sigma])
+        # 迭代过程，不断更新alpha,均值，协方差
+        gamma, current_log_loss = self.cal_gamma(X)  # 计算后验概率，隐变量Zj,PM
+        for _ in range(self.max_epochs):
+            pass
+
+
+    def cal_gamma(self, X):
+        '''
+        计算后验概率
+        :param X:
+        :return:
+        '''
+        gamma = np.array([self.gaussian_nd(X, mu, sigma) * alpha \
+                          for alpha, mu, sigma in self.params])
+        log_loss = np.sum(np.log(np.sum(gamma, axis=1))) / X.shape[0]
+        gamma = gamma / np.sum(gamma, axis=1, keepdims=True)
+        return gamma, log_loss
+
+
+if __name__ == '__main__':
+    X = pd.read_csv('../data/watermelon4.0.csv').values
+    gmm = GaussianMixtureCluster()
+    gmm.fit_gmm(X)
