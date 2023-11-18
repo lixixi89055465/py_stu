09afe18ba08044e0e138a1599b4846bf02b402db
lixixi89055465 <nanji1850094299@outlook.com>
2023-11-18 23:45:17 +0800

    machine learning

--- limu/11_p3.py
+++ limu/11_p3.py
@@ -48,7 +48,7 @@ def evaluate_loss(net, data_iter, loss):
 def train(train_features, test_features, train_labels, \
           test_labels, num_epochs=400):
     loss = nn.MSELoss()
-    input_shape = train_features.shape(-1)
+    input_shape = train_features.shape[-1]
     net = nn.Sequential(nn.Linear(input_shape, 1, bias=False))
     batch_size = min(10, train_labels.shape[0])
     train_iter = d2l.load_array((train_features, train_labels.reshape(-1, 1)), \
@@ -56,14 +56,17 @@ def train(train_features, test_features, train_labels, \
     test_iter = d2l.load_array((test_features, test_labels.reshape(-1, 1)), \
                                batch_size, is_train=False)
     trainer = torch.optim.SGD(net.parameters(), lr=0.01)
-
     animator = d2l.Animator(xlabel='epoch', ylabel='loss', yscale='log', \
                             xlim=[1, num_epochs], ylim=[1e-3, 1e2], \
                             legend=['train', 'test'])
     for epoch in range(num_epochs):
         d2l.train_epoch_ch3(net, train_iter, loss, trainer)
         if epoch == 0 or (epoch + 1) % 20 == 0:
-            animator.add(epoch + 1, (evaluate_loss( \
-                net, train_iter, loss), \
-                                     evaluate_loss(net, test_iter, loss)))
-    print('weight:' + net[0].weight.data.numpy())
+            animator.add(epoch + 1,
+                         (evaluate_loss(net, train_iter, loss),
+                          evaluate_loss(net, test_iter, loss)))
+    print('weight:', net[0].weight.data.numpy())
+
+
+train(poly_features[:n_train, :4], poly_features[n_train:, :4], \
+      labels[:n_train], labels[n_train:])
