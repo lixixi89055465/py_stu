a72b07679a170644d993e6ca6cb7c3da3675c31c
xiang.li4 <wb_xiang.li4@sinitek.com>
2023-11-22 11:31:11 +0800

    machine learning 2022

--- limu/kaggle01.py
+++ limu/kaggle01.py
@@ -3,6 +3,8 @@ import os
 import tarfile
 import zipfile
 import requests
+import torch
+from torch import nn
 
 DATA_HUB = dict()
 DATA_URL = 'http://d2l-data.s3-accelerate.amazonaws.com/'
@@ -29,3 +31,76 @@ def download(name, cache_dir=os.path.join('..', 'data')):
     with open(fname, 'wb') as f:
         f.write(r.content)
     return fname
+
+
+def download_extract(name, folder=None):  # @save
+    """下载并解压zip/tar文件"""
+    fname = download(name)
+    base_dir = os.path.dirname(fname)
+    data_dir, ext = os.path.splitext(fname)
+    if ext == '.zip':
+        fp = zipfile.ZipFile(fname, 'r')
+    elif ext in ('.tar', '.gz'):
+        fp = tarfile.open(fname, 'r')
+    else:
+        assert False, '只有zip/tar文件可以被解压缩'
+    fp.extractall(base_dir)
+    return os.path.join(base_dir, folder) if folder else data_dir
+
+
+def download_all():  # @save
+    """下载DATA_HUB中的所有文件"""
+    for name in DATA_HUB:
+        download(name)
+
+
+# %matplotlib inline
+import numpy as np
+import pandas as pd
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+DATA_HUB['kaggle_house_train'] = (  # @save
+    DATA_URL + 'kaggle_house_pred_train.csv',
+    '585e9cc93e70b39160e7921475f9bcd7d31219ce')
+
+DATA_HUB['kaggle_house_test'] = (  # @save
+    DATA_URL + 'kaggle_house_pred_test.csv',
+    'fa19780a7b011d9b009e8bff8e99922a8ee2eb90')
+train_data = pd.read_csv(download('kaggle_house_train'))
+test_data = pd.read_csv(download('kaggle_house_test'))
+
+print(train_data.shape)
+print(test_data.shape)
+print(train_data.columns)
+print(train_data.info())
+
+print(train_data.iloc[:4, [0, 1, 2, 3, -3, -2, -1]])
+all_features = pd.concat((train_data.iloc[:, 1:-1], test_data.iloc[:, 1:]))
+
+print('0' * 100)
+print(all_features.shape)
+print(test_data.shape)
+numeric_features = all_features.dtypes[all_features.dtypes != 'object'].index
+print('1' * 100)
+print(numeric_features[:10])
+all_features[numeric_features] = all_features[numeric_features].apply(lambda x: (x - x.mean()) / x.std())
+print('2' * 100)
+print(all_features.shape)
+all_features = pd.get_dummies(all_features, dummy_na=True)
+print('3' * 100)
+print(all_features.shape)
+n_train = train_data.shape[0]
+train_features = torch.tensor(all_features[:n_train].values, dtype=torch.float32)
+test_features = torch.tensor(all_features[n_train:].values, dtype=torch.float32)
+train_labels=torch.Tensor(
+    train_data.SalePrice.values.reshape(-1,1),dtype=torch.float32
+)
+loss=nn.MSELoss()
+in_features=train_features.shape[1]
+def get_net():
+    net=nn.Sequential(nn.Linear(in_features,1))
+    return net
+
+
