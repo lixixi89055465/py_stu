709d09aa722a7aea09f1227d076470519026d63d
xiang.li4 <wb_xiang.li4@sinitek.com>
2023-11-22 15:28:05 +0800

    limu

--- limu/kaggle01.py
+++ limu/kaggle01.py
@@ -86,6 +86,7 @@ numeric_features = all_features.dtypes[all_features.dtypes != 'object'].index
 print('1' * 100)
 print(numeric_features[:10])
 all_features[numeric_features] = all_features[numeric_features].apply(lambda x: (x - x.mean()) / x.std())
+all_features[numeric_features] = all_features[numeric_features].fillna(0)
 print('2' * 100)
 print(all_features.shape)
 all_features = pd.get_dummies(all_features, dummy_na=True)
@@ -94,13 +95,87 @@ print(all_features.shape)
 n_train = train_data.shape[0]
 train_features = torch.tensor(all_features[:n_train].values, dtype=torch.float32)
 test_features = torch.tensor(all_features[n_train:].values, dtype=torch.float32)
-train_labels=torch.Tensor(
-    train_data.SalePrice.values.reshape(-1,1),dtype=torch.float32
+train_labels = torch.tensor(
+    train_data.SalePrice.values.reshape(-1, 1), dtype=torch.float32
 )
-loss=nn.MSELoss()
-in_features=train_features.shape[1]
+loss = nn.MSELoss()
+in_features = train_features.shape[1]
+
+
 def get_net():
-    net=nn.Sequential(nn.Linear(in_features,1))
+    net = nn.Sequential(nn.Linear(in_features, 1))
     return net
 
 
+def log_rmse(net, features, labels):
+    # 为了在取对数时进一步稳定该值，将小于1的值设置为1
+    result = net(features)
+    clipped_preds = torch.clamp(result, 1, float('inf'))
+    rmse = torch.sqrt(loss(torch.log(clipped_preds), \
+                           torch.log(labels)))
+    return rmse.item()
+
+
+def train(net, train_features, train_labels, test_features, test_labels, \
+          num_epochs, learning_rate, weight_decay, batch_size):
+    train_ls, test_ls = [], []
+    train_iter = d2l.load_array((train_features, train_labels), batch_size)
+    # 这里使用的是Adam优化算法
+    optimizer = torch.optim.Adam(net.parameters(), \
+                                 lr=learning_rate, \
+                                 weight_decay=weight_decay)
+    for epoch in range(num_epochs):
+        for X,y in train_iter:
+            optimizer.zero_grad()
+            l=loss(net(X),y)
+            l.backward()
+            optimizer.step()
+        train_ls.append(log_rmse(net,train_features,train_labels))
+        if test_labels is not None:
+            test_ls.append(log_rmse(net,test_features,test_labels))
+    return train_ls,test_ls
+
+def get_k_fold_data(k, i, X, y):
+    assert k > 1
+    fold_size = X.shape[0] // k
+    X_train, y_train = None, None
+    for j in range(k):
+        idx = slice(j * fold_size, (j + 1) * fold_size)
+        X_part, y_part = X[idx, :], y[idx, :]
+        if j == i:
+            X_valid, y_valid = X_part, y_part
+        elif X_train is None:
+            X_train, y_train = X_part, y_part
+        else:
+            X_train = torch.cat([X_train, X_part], 0)
+            y_train = torch.cat([y_train, y_part], 0)
+    return X_train, y_train, X_valid, y_valid
+
+
+def k_fold(k, X_train, y_train, num_epochs, learning_rate, \
+           weight_decay, batch_size):
+    train_ls_sum, valid_l_sum = 0, 0
+    for i in range(k):
+        data = get_k_fold_data(k, i, X_train, y_train)
+        net = get_net()
+        train_ls, valid_ls = train(net, *data, num_epochs, \
+                                   learning_rate, \
+                                   weight_decay, \
+                                   batch_size)
+        train_ls_sum += train_ls[-1]
+        valid_l_sum += valid_ls[-1]
+        if i == 0:
+            d2l.plot(list(range(1, num_epochs + 1)), [train_ls, valid_ls], \
+                     xlabel='epoch', ylabel='rmse', \
+                     xlim=[1, num_epochs], \
+                     legend=['train', 'valid'], yscale='log')
+        print(f'折{i + 1},训练log rmse {float(train_ls[-1]):f},' \
+                  f'验证log rmse {float(valid_ls[-1]):f}')
+    return train_ls_sum / k, valid_l_sum / k
+
+
+k, num_epochs, lr, weight_decay, batch_size = 5, 100,5, 0, 64
+train_l, valid_l = k_fold(k, train_features, train_labels, num_epochs, lr,
+                          weight_decay, batch_size)
+print(f'{k}-折验证: 平均训练log rmse: {float(train_l):f}, '
+      f'平均验证log rmse: {float(valid_l):f}')
