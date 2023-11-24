b91fc4027c5276636e09253aa4c3fb91d5bcc12f
lixixi89055465 <nanji1850094299@outlook.com>
2023-11-24 23:03:50 +0800

    machine learning

--- limu/exam02/17_p1.py
+++ limu/exam02/17_p1.py
@@ -8,14 +8,14 @@ print(torch.cuda.device_count())
 
 def try_gpu(i=0):
     '''如果存在，则返回gpu(i),否则返回cpu().'''
-    if torch.cuda.device_count() > i + 1:
+    if torch.cuda.device_count() >= i + 1:
         return torch.device(f'cuda:{i}')
     return torch.device('cpu')
 
 
 def try_all_gpus():
     devices = [
-        torch.device(f'cuda:{i}' for i in range(torch.cuda.device_count()))
+        torch.device(f'cuda:{i}') for i in range(torch.cuda.device_count())
     ]
     return devices if devices else [torch.device('cpu')]
 
@@ -25,6 +25,14 @@ print('2' * 100)
 print(x.device)
 print('3' * 100)
 # x = torch.ones(2, 3, device=try_gpu())
-x = torch.ones(2, 3, device=try_all_gpus())
+x = torch.ones(2, 3, device=try_gpu())
 print('4' * 100)
 print(x)
+print('5' * 100)
+net = nn.Sequential(nn.Linear(3, 1))
+net = net.to(device=try_gpu())
+print('6' * 100)
+print(net(x))
+print('7'*100)
+print(net[0].weight.data.device)
+print('8'*100)
