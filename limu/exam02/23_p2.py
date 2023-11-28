00d54e14c0e93ea552a7a124e9f520197ddd6165
nanji <1850094299@qq.com>
2023-11-28 23:36:39 +0800

    limu

--- limu/exam02/23_p2.py
+++ limu/exam02/23_p2.py
@@ -18,9 +18,15 @@ class Reshape(torch.nn.Module):
 net = nn.Sequential(
     Reshape(), nn.Conv2d(1, 6, kernel_size=5, padding=2),
     nn.AvgPool2d(kernel_size=2, stride=2), nn.Conv2d(6, 16, kernel_size=5), \
-    nn.Sigmoid(), nn.AvgPool2d(kernel_size=2, stride=2), nn.Flatten(),
-    nn.Linear(16 * 5 * 5, 120), nn.Sigmoid(),
-    nn.Linear(120, 84), nn.Sigmoid(),
+    # nn.Sigmoid(),
+    nn.ReLU(),
+    nn.AvgPool2d(kernel_size=2, stride=2), nn.Flatten(),
+    nn.Linear(16 * 5 * 5, 120),
+    nn.ReLU(),
+    # nn.Sigmoid(),
+    nn.Linear(120, 84),
+    nn.ReLU(),
+    nn.Sigmoid(),
     nn.Linear(84, 10)
 )
 X = torch.rand(size=(1, 1, 28, 28), dtype=torch.float32)
---
+++ limu/exam02/24_p2.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/11/28 下午10:05
+# @Author  : nanji
+# @Site    : 
+# @File    : 24_p2.py
+# @Software: PyCharm 
+# @Comment :
+import torch
+from torch import nn
+from d2l import torch as d2l
+import matplotlib.pyplot as plt
+
+net = nn.Sequential(
+    nn.Conv2d(1, 96, kernel_size=11, stride=4, padding=1), nn.ReLU(),
+    # 1*224*224=>96*54*54
+    nn.MaxPool2d(kernel_size=3, stride=2),
+    # 96*26*26
+    nn.Conv2d(96, 256, kernel_size=5, padding=2), nn.ReLU(),
+    # 256*26*26
+    nn.MaxPool2d(kernel_size=3, stride=2),
+    # 256*12*12
+    nn.Conv2d(256, 384, kernel_size=3, padding=1), nn.ReLU(),
+    # 384*12*12
+    nn.Conv2d(384, 384, kernel_size=3, padding=1), nn.ReLU(),
+    # 384*12*12
+    nn.Conv2d(384, 256, kernel_size=3, padding=1), nn.ReLU(),
+    # 256*12*12
+    nn.MaxPool2d(kernel_size=3, stride=2),
+    # 256*5*5
+    nn.Flatten(),
+    # 6400
+    nn.Linear(6400, 4096), nn.ReLU(),
+    nn.Dropout(p=0.5),
+    nn.Linear(4096, 4096), nn.ReLU(),
+    nn.Dropout(p=0.5),
+    nn.Linear(4096, 10)
+)
+
+X = torch.randn(1, 1, 224, 224)
+for layer in net:
+    X = layer(X)
+    print(layer.__class__.__name__, 'Output shape:\t', X.shape)
+
+batch_size = 128
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size, resize=224)
+lr, num_epochs = 0.01, 1
+d2l.train_ch6(net, train_iter, test_iter, num_epochs, lr, d2l.try_gpu())
+plt.savefig('1.png')
