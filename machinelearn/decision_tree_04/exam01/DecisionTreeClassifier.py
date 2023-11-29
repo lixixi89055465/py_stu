deb884ee851f3a6f6bacaf81b56a2af9e11bf7b4
xiang.li4 <wb_xiang.li4@sinitek.com>
2023-11-29 15:25:35 +0800

    machine learning

---
+++ machinelearn/decision_tree_04/exam01/DecisionTreeClassifier.py
@@ -0,0 +1,5 @@
+
+class DecisionTreeClassifier:
+    '''
+    决策树分类算法：包括ID3、C4.5和CART树，后剪技处理，针对连续特征数据，进行分箱处理
+    '''
--- machinelearn/decision_tree_04/exam01/TreeNode.py
+++ machinelearn/decision_tree_04/exam01/TreeNode.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class TreeNode(object):
     '''
     树节点实体类封装，略去get和setX方法，用于存储节点信息以及关联子节点，
@@ -22,3 +25,55 @@ class TreeNode(object):
 
     def level_order(self):
         pass
+
+    def cal_gini(self, y, sample_weight=None):
+        '''
+        计算基尼系数
+        :param y:
+        :param sample_weight:
+        :return:
+        '''
+        y = np.asarray(y)
+        sample_weight = self._set_sample_weight(sample_weight, len(y))
+        y_values = np.unique(y)
+        gini = 1.0
+        for val in y_values:
+            p_i = 1.0 * len(y[val == y]) * np.mean(sample_weight[y == val]) / len(y)
+            gini -= p_i * p_i
+        return gini
+
+    def conditional_gini(self, feature_x, y_labels, sample_weight=None):
+        '''
+        计算条件gini系数 ：Gini(y|x)
+        :param feature_x:
+        :param y_labels:
+        :param sample_weight:
+        :return:
+        '''
+        x, y = np.asarray(feature_x), np.asarray(y_labels)
+        sample_weight = self._set_sample_weight(sample_weight)
+        cond_gini = .0  # 计算条件 gini系数
+        for x_val in np.unique(x):
+            x_idx = np.where(x_val == x)
+            sub_x, sub_y, sub_sample_weight = x[x_idx], y[x_idx], sample_weight[x_idx]
+            p_i = 1.0 * len(x_idx) / len(x)
+            cond_gini += p_i * self.cal_gini(sub_y, sub_sample_weight)
+        return cond_gini
+
+    def gini_gain(self, feature_x, y_labels, sample_weight=None):
+        '''
+        计算gini值的增益Gini(D) -Gini(y|x)
+        :param feature_x:
+        :param y_labels:
+        :param sample_weight:
+        :return:
+        '''
+        g_gain = self.cal_gini(y_labels, sample_weight) - \
+                 self.conditional_gini(feature_x, y_labels, sample_weight)
+        return g_gain
+
+
+    def _set_sample_weight(self, sample_weight, n_sample):
+        if sample_weight is None:
+            sample_weight = [1.0] * n_sample
+        return sample_weight
