d1d3e5cf53b1df534cc3b5fd0bce99f5be6d8a98
xiang.li4 <wb_xiang.li4@sinitek.com>
2023-12-01 16:52:22 +0800

    machine learning

--- limu/exam03/27_p2.py
+++ limu/exam03/27_p2.py
@@ -19,7 +19,7 @@ class Inception(nn.Module):
         self.p2_2 = nn.Conv2d(c2[0], c2[1], kernel_size=3, padding=1)
         self.p3_1 = nn.Conv2d(in_channels, c3[0], kernel_size=1)
         self.p3_2 = nn.Conv2d(c3[0], c3[1], kernel_size=5, padding=2)
-        self.p4_1 = nn.MaxPool2d(kernel_size=3,stride=1,padding=1)
+        self.p4_1 = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
         self.p4_2 = nn.Conv2d(in_channels, c4, kernel_size=1)
 
     def forward(self, x):
@@ -29,7 +29,50 @@ class Inception(nn.Module):
         p4 = F.relu(self.p4_2(self.p4_1(x)))
         return torch.cat((p1, p2, p3, p4), dim=1)
 
-b1=nn.Sequential(
-    nn.Conv2d(1,64,kernel_size=7,stride=2,padding=3),
-    nn.ReLU(),nn
+
+# 1*224*224
+b1 = nn.Sequential(
+    nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3), nn.ReLU(),
+    # 64*112*112
+    nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+    # 64*56*56
 )
+b2 = nn.Sequential(
+    nn.Conv2d(64, 64, kernel_size=1),
+    # 64*56*56
+    nn.ReLU(),
+    nn.Conv2d(64, 192, kernel_size=3, padding=1),
+    # 192*56*56
+    nn.ReLU(),
+    nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+    # 192*28*28
+)
+b3 = nn.Sequential(
+    Inception(192, 64, (96, 128), (16, 32), 32),
+    Inception(256, 128, (128, 192), (32, 96), 64),
+    nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+)
+b4 = nn.Sequential(
+    Inception(480, 192, (96, 208), (16, 48), 64),
+    Inception(512, 160, (112, 224), (24, 64), 64),
+    Inception(512, 128, (128, 256), (24, 64), 64),
+    Inception(512, 112, (114, 288), (32, 64), 64),
+    Inception(528, 256, (160, 320), (32, 128), 128),
+    nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+)
+
+b5 = nn.Sequential(
+    Inception(832, 256, (160, 320), (32, 128), 128),
+    Inception(832, 384, (192, 384), (48, 128), 128),
+    nn.AdaptiveAvgPool2d((1, 1)),
+    nn.Flatten()
+)
+net = nn.Sequential(b1, b2, b3, b4, b5, nn.Linear(1024, 10))
+x = torch.rand(size=(1, 1, 224, 224))
+for layer in net:
+    x = layer(x)
+    print(layer.__class__.__name__, 'output size :\t ', x.shape)
+
+lr, num_epochs, batch_size = 0.1, 10, 128
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size, resize=96)
+d2l.train_ch6(net, train_iter, test_iter, num_epochs, lr, d2l.try_gpu())
