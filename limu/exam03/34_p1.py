e4bcad675d4cca2e886c552aafe4295e8b7293df
lixiang <1850094299@qq.com>
2023-12-02 19:52:19 +0800

    limu

---
+++ limu/exam03/34_p1.py
@@ -0,0 +1,19 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+from d2l import torch as d2l
+
+scale = 0.01
+W1 = torch.randn(size=(20, 1, 3, 3)) * scale
+b1 = torch.zeros(20)
+W2 = torch.randn(size=(50, 20, 5, 5)) * scale
+b2 = torch.zeros(50)
+W3 = torch.randn(size=(800, 128)) * scale
+b3 = torch.zeros(128)
+W4 = torch.randn(size=(128, 10)) * scale
+b4 = torch.zeros(10)
+params = [W1, b1, W2, b2, W3, b3, W4, b4]
+
+
+def lenet(X, params):
+    h1_conv=F.conv2d(input=X,weight=params[0],bias=params[1])
