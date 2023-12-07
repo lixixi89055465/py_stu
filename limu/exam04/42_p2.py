e869d578345a834928702639b5df22dab2e11332
nanji <1850094299@qq.com>
2023-12-08 00:12:05 +0800

    limu

--- limu/exam04/42_p2.py
+++ limu/exam04/42_p2.py
@@ -22,7 +22,7 @@ def multibox_prior(data, sizes, ratios):
 	in_height, in_width = data.shape[-2:]
 	device, num_sizes, num_ratio = data.device, len(sizes), len(ratios)
 	boxes_per_pixel = (num_sizes + num_ratio - 1)
-	size_tensor = torch.torsor(sizes, device=device)
+	size_tensor = torch.tensor(sizes, device=device)
 	ratio_tensor = torch.tensor(ratios, device=device)
 	# 为了将锚点移动到像素的中心，需要设置偏移量。
 	# 因为一个像素的搞为1且宽为1，我们选择偏移我们的中心0.5
@@ -31,16 +31,16 @@ def multibox_prior(data, sizes, ratios):
 	steps_w = 1.0 / in_width  # 在x轴上缩放步长
 
 	# 生成锚框的所有中心点
-	center_h = (torch.arange(in_height, device) + offset_h) * steps_h
-	center_w = (torch.arange(in_width, device) + offset_w) * steps_w
-	shift_y, shift_x = torch.meshgrid(center_h, center_w)
+	center_h = (torch.arange(in_height, device=device) + offset_h) * steps_h
+	center_w = (torch.arange(in_width, device=device) + offset_w) * steps_w
+	shift_y, shift_x = torch.meshgrid(center_h, center_w, indexing='ij')
 	shift_y, shift_x = shift_y.reshape(-1), shift_x.reshape(-1)
 
 	# 生成 boxes_per_pixel 个高和宽 ,
 	w = torch.cat((size_tensor * torch.sqrt(ratio_tensor[0]),
 				   sizes[0] * torch.sqrt(ratio_tensor[1:]))) * in_height / in_width
 	h = torch.cat((size_tensor / torch.sqrt(ratio_tensor[0]), \
-				   sizes[0 / torch.sqrt(ratio_tensor[1:])]))
+				   sizes[0] / torch.sqrt(ratio_tensor[1:])))
 	# 除以 2来获得半高和半宽
 	anchor_manipulations = torch.stack((-w, -h, w, h)).T.repeat(in_height * in_width, 1) / 2
 	# 每个中心点都将有 boxes_per_pixel个锚框
@@ -49,3 +49,64 @@ def multibox_prior(data, sizes, ratios):
 		.repeat_interleave(boxes_per_pixel, dim=0)
 	output = out_grid + anchor_manipulations
 	return output.unsqueeze(0)
+
+
+img = d2l.plt.imread('../img/catdog.jpg')
+h, w = img.shape[:2]
+print(h, w)
+X = torch.rand(size=(1, 3, h, w))
+Y = multibox_prior(X, sizes=[0.75, 0.5, 0.25], ratios=[1, 2, 0.5])
+print(Y.shape)
+
+boxes = Y.reshape(h, w, 5, 4)
+print('0' * 100)
+print(boxes[250, 250, 0, :])
+#@save
+def show_bboxes(axes, bboxes, labels=None, colors=None):
+    """显示所有边界框"""
+    def _make_list(obj, default_values=None):
+        if obj is None:
+            obj = default_values
+        elif not isinstance(obj, (list, tuple)):
+            obj = [obj]
+        return obj
+
+    labels = _make_list(labels)
+    colors = _make_list(colors, ['b', 'g', 'r', 'm', 'c'])
+    for i, bbox in enumerate(bboxes):
+        color = colors[i % len(colors)]
+        rect = d2l.bbox_to_rect(bbox.detach().numpy(), color)
+        axes.add_patch(rect)
+        if labels and len(labels) > i:
+            text_color = 'k' if color == 'w' else 'w'
+            axes.text(rect.xy[0], rect.xy[1], labels[i],
+                      va='center', ha='center', fontsize=9, color=text_color,
+                      bbox=dict(facecolor=color, lw=0))
+d2l.set_figsize()
+bbox_scale = torch.tensor((w, h, w, h))
+fig = d2l.plt.imshow(img)
+show_bboxes(fig.axes, boxes[250, 250, :, :] * bbox_scale,
+            ['s=0.75, r=1', 's=0.5, r=1', 's=0.25, r=1', 's=0.75, r=2',
+             's=0.75, r=0.5'])
+
+#@save
+def box_iou(boxes1, boxes2):
+    """计算两个锚框或边界框列表中成对的交并比"""
+    box_area = lambda boxes: ((boxes[:, 2] - boxes[:, 0]) *
+                              (boxes[:, 3] - boxes[:, 1]))
+    # boxes1,boxes2,areas1,areas2的形状:
+    # boxes1：(boxes1的数量,4),
+    # boxes2：(boxes2的数量,4),
+    # areas1：(boxes1的数量,),
+    # areas2：(boxes2的数量,)
+    areas1 = box_area(boxes1)
+    areas2 = box_area(boxes2)
+    # inter_upperlefts,inter_lowerrights,inters的形状:
+    # (boxes1的数量,boxes2的数量,2)
+    inter_upperlefts = torch.max(boxes1[:, None, :2], boxes2[:, :2])
+    inter_lowerrights = torch.min(boxes1[:, None, 2:], boxes2[:, 2:])
+    inters = (inter_lowerrights - inter_upperlefts).clamp(min=0)
+    # inter_areasandunion_areas的形状:(boxes1的数量,boxes2的数量)
+    inter_areas = inters[:, :, 0] * inters[:, :, 1]
+    union_areas = areas1[:, None] + areas2 - inter_areas
+    return inter_areas / union_areas
