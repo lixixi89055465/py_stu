47fd332580da596f312351e0b646dbe3c70b4d44
nanji <1850094299@qq.com>
2023-12-10 18:11:52 +0800

    limu

--- limu/exam04/testtorchsummary.py
+++ limu/exam04/testtorchsummary.py
@@ -5,3 +5,11 @@
 # @File    : testtorchsummary.py
 # @Software: PyCharm 
 # @Comment :
+from torchsummary import summary
+from torchvision.models import vgg16
+import torch
+
+device=torch.device('cuda:0')
+myNet = vgg16().to(device)
+print('0' * 100)
+summary(myNet,input_size=(3,64,64))
