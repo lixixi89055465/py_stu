41d66450cb286efc35e73802e5978b28b16e86d1
lixixi89055465 <1850094299@qq.com>
2023-12-18 23:13:40 +0800

    limu

--- limu/exam05/53_2.py
+++ limu/exam05/53_2.py
@@ -11,5 +11,46 @@ from d2l import torch as d2l
 
 tokens = d2l.tokenize(d2l.read_time_machine())
 corpus = [token for line in tokens for token in line]
-vocab=d2l.Vocab(corpus)
+vocab = d2l.Vocab(corpus)
 print(vocab.token_freqs[:10])
+
+freqs = [freq for token, freq in vocab.token_freqs]
+d2l.plot(freqs, xlabel='token:x', ylabel='frequency: n(x)', xscale='log', yscale='log')
+
+bigram_tokens = [pair for pair in zip(corpus[:-1], corpus[1:])]
+bigram_vocab = d2l.Vocab(bigram_tokens)
+
+trigram_tokens = [triple for triple in zip(corpus[:-2], corpus[1:-1], corpus[2:])]
+trigram_vocab = d2l.Vocab(trigram_tokens)
+
+bigram_freqs = [freq for token, freq in bigram_vocab.token_freqs]
+trigram_freqs = [freq for token, freq in trigram_vocab.token_freqs]
+d2l.plot([freqs, bigram_freqs, trigram_freqs],
+         xlabel='token:x',
+         ylabel='frequency: n(x) ',
+         xscale='log', yscale='log',
+         legend=['unigram', 'bigram', 'trigram'])
+
+
+def seq_data_iter_random(corpus, batch_size, num_steps):
+    '''使用随机抽样生成一个小批量子序列 '''
+    corpus = corpus[random.randint(0, num_steps - 1):]
+    num_subseqs = (len(corpus) - 1) // num_steps
+    initial_indices = list(range(0, num_subseqs * num_steps, num_steps))
+    random.shuffle(initial_indices)
+
+    def data(pos):
+        return corpus[pos:pos + num_steps]
+
+    num_batches = num_subseqs // batch_size
+    for i in range(0, batch_size * num_batches, batch_size):
+        initial_indices_per_batch = initial_indices[i:i + batch_size]
+        X = [data(j) for j in initial_indices_per_batch]
+        Y = [data(j + 1) for j in initial_indices_per_batch]
+        yield torch.tensor(X), torch.tensor(Y)
+
+
+my_seq = list(range(35))
+print('5' * 100)
+for X, Y in seq_data_iter_random(my_seq, batch_size=2, num_steps=5):
+    print('X:', X, '\nY: ', Y)
