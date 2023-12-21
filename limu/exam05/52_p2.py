bf77af10ac34f1136b551c38f240c0f5acd3cddf
xiang.li4 <wb_xiang.li4@sinitek.com>
2023-12-21 14:04:58 +0800

    limu

--- limu/exam05/52_p2.py
+++ limu/exam05/52_p2.py
@@ -11,14 +11,14 @@ from d2l import torch as d2l
 
 # @save
 d2l.DATA_HUB['time_machine'] = (d2l.DATA_URL + 'timemachine.txt',
-                                '090b5e7e70c295757f55df93cb0a180b9691891a')
+								'090b5e7e70c295757f55df93cb0a180b9691891a')
 
 
 def read_time_machine():  # @save
-    """将时间机器数据集加载到文本行的列表中"""
-    with open(d2l.download('time_machine'), 'r') as f:
-        lines = f.readlines()
-    return [re.sub('[^A-Za-z]+', ' ', line).strip().lower() for line in lines]
+	"""将时间机器数据集加载到文本行的列表中"""
+	with open(d2l.download('time_machine'), 'r') as f:
+		lines = f.readlines()
+	return [re.sub('[^A-Za-z]+', ' ', line).strip().lower() for line in lines]
 
 
 lines = read_time_machine()
@@ -28,103 +28,85 @@ print(lines[10])
 
 
 def tokenize(lines, token='word'):
-    '''将文本行拆分位单词或字符标记 '''
-    if token == 'word':
-        return [line.split() for line in lines]
-    elif token == 'char':
-        return [list(line) for line in lines]
-    else:
-        print('错误：未知令牌类型:' + token)
+	'''将文本行拆分位单词或字符标记 '''
+	if token == 'word':
+		return [line.split() for line in lines]
+	elif token == 'char':
+		return [list(line) for line in lines]
+	else:
+		print('错误：未知令牌类型:' + token)
 
 
 class Vocab:  # @save
-    """文本词表"""
+	"""文本词表"""
 
-    def __init__(self, tokens=None, min_freq=0, reserved_tokens=None):
-        if tokens is None:
-            tokens = []
-        if reserved_tokens is None:
-            reserved_tokens = []
-        # 按出现频率排序
-        counter = count_corpus(tokens)
-        self._token_freqs = sorted(counter.items(), key=lambda x: x[1],
-                                   reverse=True)
-        # 未知词元的索引为0
-        self.idx_to_token = ['<unk>'] + reserved_tokens
-        self.token_to_idx = {token: idx
-                             for idx, token in enumerate(self.idx_to_token)}
-        for token, freq in self._token_freqs:
-            if freq < min_freq:
-                break
-            if token not in self.token_to_idx:
-                self.idx_to_token.append(token)
-                self.token_to_idx[token] = len(self.idx_to_token) - 1
+	def __init__(self, tokens=None, min_freq=0, reserved_tokens=None):
+		if tokens is None:
+			tokens = []
+		if reserved_tokens is None:
+			reserved_tokens = []
+		counter = count_corpus(tokens)
+		self._token_freqs = sorted(counter.items(), key=lambda x: x[1], \
+								   reverse=True)
+		self.idx_to_token = ['<unk>'] + reserved_tokens
+		self.token_to_idx = {token: idx
+							 for idx, token in enumerate(self.idx_to_token)}
+		for token, freq in self._token_freqs:
+			if freq < min_freq:
+				break
+			if token not in self.token_to_idx:
+				self.idx_to_token.append(token)
+				self.token_to_idx[token] = len(self.idx_to_token) - 1
 
-    def __len__(self):
-        return len(self.idx_to_token)
+	def __len__(self):
+		return len(self.idx_to_token)
 
-    def __getitem__(self, tokens):
-        if not isinstance(tokens, (list, tuple)):
-            return self.token_to_idx.get(tokens, self.unk)
-        return [self.__getitem__(token) for token in tokens]
+	def __getitem__(self, tokens):
+		if not isinstance(tokens, (list, tuple)):
+			return self.token_to_idx.get(tokens, self.unk)
+		return [self.__getitem__(token) for token in tokens]
 
-    def to_tokens(self, indices):
-        if not isinstance(indices, (list, tuple)):
-            return self.idx_to_token[indices]
-        return [self.idx_to_token[index] for index in indices]
+	def to_tokens(self, indices):
+		if not isinstance(indices, (list, tuple)):
+			return self.idx_to_token[indices]
+		return [self.idx_to_token[index] for index in indices]
 
-    @property
-    def unk(self):  # 未知词元的索引为0
-        return 0
+	@property
+	def unk(self):  # 未知词元的索引为0
+		return 0
 
-    @property
-    def token_freqs(self):
-        return self._token_freqs
+	@property
+	def token_freqs(self):
+		return self._token_freqs
 
-    def to_tokens(self, indices):
-        if not isinstance(indices, (list, tuple)):
-            return self.idx_to_token[indices]
-        return [self.idx_to_token[index] for index in indices]
+	def to_tokens(self, indices):
+		if not isinstance(indices, (list, tuple)):
+			return self.idx_to_token[indices]
+		return [self.idx_to_token[index] for index in indices]
 
 
 def count_corpus(tokens):  # @save
-    """统计词元的频率"""
-    # 这里的tokens是1D列表或2D列表
-    if len(tokens) == 0 or isinstance(tokens[0], list):
-        # 将词元列表展平成一个列表
-        tokens = [token for line in tokens for token in line]
-    return collections.Counter(tokens)
+	"""统计词元的频率"""
+	if len(tokens) == 0 or isinstance(tokens[0], list):
+		tokens = [token for line in tokens for token in line]
+	return collections.Counter(tokens)
 
 
 def load_corpus_time_machine(max_tokens=-1):
-    '''
-    返回时光机器数据集的标记索引列表和词汇表
-    :param max_tokens:
-    :return:
-    '''
-    lines = read_time_machine()
-    tokens = tokenize(lines, 'char')
-    vocab = Vocab(tokens)
-    corpus = [vocab[token] for line in tokens for token in line]
-    if max_tokens > 0:
-        corpus = corpus[:max_tokens]
-    return corpus, vocab
-
+	'''
+	返回时光机器数据集的标记索引列表和词汇表
+	:param max_tokens:
+	:return:
+	'''
+	lines = read_time_machine()
+	tokens = tokenize(lines, 'word')
+	vocab = Vocab(tokens)
+	corpus = [vocab[token] for line in tokens for token in line]
+	if max_tokens > 0:
+		corpus = corpus[:max_tokens]
+	return corpus, vocab
 
 
 if __name__ == '__main__':
-    tokens = tokenize(lines)
-    for i in range(11):
-        print(tokens[i])
-
-    vocab = Vocab(tokens)
-    print('0' * 100)
-    print(list(vocab.token_to_idx.items())[:10])
-
-    print('1' * 100)
-    for i in [0, 10]:
-        print('words:', tokens[i])
-        print('indices:', vocab[tokens[i]])
-    print('2'*100)
-    corpus,vocab=load_corpus_time_machine()
-    print(len(corpus), len(vocab))
+	corpus, vocab = load_corpus_time_machine()
+	print(len(corpus), len(vocab))
--- limu/exam05/55_p1.py
+++ limu/exam05/55_p1.py
@@ -16,68 +16,83 @@ train_iter, vocab = d2l.load_data_time_machine(batch_size, num_steps)
 
 print(F.one_hot(torch.tensor([0, 2]), len(vocab)))
 X = torch.arange(10).reshape((2, 5))
-X = F.one_hot(X.T, 28)
+# X = F.one_hot(X.T, 28)
 print('0' * 100)
 print(X.type)
-print(X[0, :, :])
 
 
 def get_params(vocab_size, num_hiddens, device):
-    num_inputs = num_outputs = vocab_size
+	num_inputs = num_outputs = vocab_size
 
-    def normal(shape):
-        return torch.randn(size=shape, device=device) * 0.01
+	def normal(shape):
+		return torch.randn(size=shape, device=device) * 0.01
 
-    W_xh = normal((num_inputs, num_hiddens))
-    W_hh = normal((num_hiddens, num_hiddens))
-    b_h = torch.zeros(num_hiddens, device=device)
-    # 输出层参数
-    W_hq = normal((num_hiddens, num_outputs))
-    b_q = torch.zeros(num_outputs, device=device)
-    params = [W_xh, W_hh, b_h, W_hq, b_q]
-    for param in params:
-        param.requires_grad_(True)
-    return params
+	W_xh = normal((num_inputs, num_hiddens))
+	W_hh = normal((num_hiddens, num_hiddens))
+	b_h = torch.zeros(num_hiddens, device=device)
+	W_hq = normal((num_hiddens, num_outputs))
+	b_q = torch.zeros(num_outputs, device=device)
+	params = [W_xh, W_hh, b_h, W_hq, b_q]
+	for param in params:
+		param.requires_grad_(True)
+	return params
 
 
 def init_rnn_state(batch_size, num_hidden, device):
-    return (torch.zeros((batch_size, num_hidden), device=device))
+	return (torch.zeros((batch_size, num_hiddens), device=device),)
 
 
 def rnn(inputs, state, params):
-    W_xh, W_hh, b_h, W_hq, b_q = params
-    H, = state
-    outputs = []
-    for X in inputs:
-        H = torch.tanh(torch.mm(X, W_xh) + \
-                       torch.mm(H, W_hh) + \
-                       b_h)
-        Y = torch.mm(H, W_hq) + b_q
-        outputs.append(Y)
-    return torch.cat(outputs, dim=0), (H,)
+	W_xh, W_hh, b_h, W_hq, b_q = params
+	H, = state
+	outputs = []
+	# X 的形状:(批量大小，词表大小
+	for X in inputs:
+		H = torch.tanh(torch.mm(X, W_xh) + torch.mm(H, W_hh) + b_h)
+		Y = torch.mm(H, W_hq) + b_q
+		outputs.append(Y)
+	return torch.cat(outputs, dim=0), (H,)
 
 
 class RNNModeScratch:
-    '''从零开始实现的循环神经网络模型 '''
+	'''从零开始实现的循环神经网络模型 '''
 
-    def __init__(self, vocab_size, num_hiddens, device, \
-                 get_params, init_state, forward_fn):
-        self.vocab_size, self.num_hiddens = vocab_size, num_hiddens
-        self.params = get_params(vocab_size, num_hiddens, device)
-        self.init_state, self.forward_fn = init_state, forward_fn
+	def __init__(self, vocab_size, num_hiddens, device, \
+				 get_params, init_state, forward_fn):
+		self.vocab_size, self.num_hidden = vocab_size, num_hiddens
+		self.params = get_params(vocab_size, num_hiddens, device)
+		self.init_state, self.forward_fn = init_state, forward_fn
 
-    def __call__(self, X, state):
-        X = F.one_hot(X.T, self.vocab_size).type(torch.float32)
-        return self.forward_fn(X, state, self.params)
+	def __call__(self, X, state):
+		X = F.one_hot(X.T, self.vocab_size).type(torch.float32)
+		return self.forward_fn(X, state, self.params)
 
-    def begin_state(self, batch_size, device):
-        return self.init_state(batch_size, self.num_hiddens, device)
+	def begin_state(self, batch_size, device):
+		return self.init_state(batch_size, self.num_hidden, device)
 
 
 num_hiddens = 512
 net = RNNModeScratch(len(vocab), num_hiddens, d2l.try_gpu(), \
-                     get_params,
-                     init_rnn_state, rnn)
-state = net.begin_state(X.shape[0],d2l.try_gpu())
+					 get_params,
+					 init_rnn_state, rnn)
+state = net.begin_state(X.shape[0], d2l.try_gpu())
 Y, new_state = net(X.to(d2l.try_gpu()), state)
 print(Y.shape, len(new_state), new_state[0].shape)
+
+
+def predict_ch8(prefix, num_preds, net, vocab, device):
+	'''在prefix后面生成新字符 '''
+	state = net.begin_state(batch_size=1, device=device)
+	outputs = [vocab[prefix[0]]]
+	get_input = lambda: torch.tensor([outputs[-1]], device=device).reshape((1, 1))
+	for y in prefix[1:]:
+		_, state = net(get_input(), state)
+		outputs.append(vocab[y])
+	for _ in range(num_preds):
+		y, state = net(get_input(), state)
+		outputs.append(int(y.argmax(dim=1).reshape(1)))
+	return ''.join([vocab.idx_to_token[i] for i in outputs])
+
+
+predict_ch8('time traveller', 10, net, vocab, d2l.try_gpu())
+
