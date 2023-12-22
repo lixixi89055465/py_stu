f6d493e11d5c769031a0ad882bc89ce4713f5445
xiang.li4 <wb_xiang.li4@sinitek.com>
2023-12-22 17:11:38 +0800

    limu

--- limu/cowboy/exam02/cowboy_object_detection_YOLOv5.py
+++ limu/cowboy/exam02/cowboy_object_detection_YOLOv5.py
@@ -49,23 +49,120 @@ def cc2yolo_bbox(img_width, img_height, bbox):
 
 
 # transfer the annotation, and generated a train dataframe file
-f = open('train.csv', 'w')
-f.write('id,file_name\n')
-for i in tqdm(range(len(data['images']))):
-	filename = data['images'][i]['file_name']
-	img_width = data['images'][i]['width']
-	img_height = data['images'][i]['height']
-	img_id = data['images'][i]['id']
-	yolo_txt_name = filename.split('.')[0] + '.txt'  # remove .jpg
+# f = open('train.csv', 'w')
+# f.write('id,file_name\n')
+# for i in tqdm(range(len(data['images']))):
+# 	filename = data['images'][i]['file_name']
+# 	img_width = data['images'][i]['width']
+# 	img_height = data['images'][i]['height']
+# 	img_id = data['images'][i]['id']
+# 	yolo_txt_name = filename.split('.')[0] + '.txt'  # remove .jpg
+#
+# 	f.write('{},{}\n'.format(img_id, filename))
+# 	yolo_txt_file = open(os.path.join(yolo_anno_path, yolo_txt_name), 'w')
+#
+# 	for anno in data['annotations']:
+# 		if anno['image_id'] == img_id:
+# 			yolo_bbox = cc2yolo_bbox(img_width, img_height, anno['bbox'])  # "bbox": [x,y,width,height]
+# 			yolo_txt_file.write(
+# 				'{} {} {} {} {}\n'.format(cate_id_map[anno['category_id']], yolo_bbox[0], yolo_bbox[1], yolo_bbox[2],
+# 										  yolo_bbox[3]))
+# 	yolo_txt_file.close()
+# f.close()
 
-	f.write('{},{}\n'.format(img_id, filename))
-	yolo_txt_file = open(os.path.join(yolo_anno_path, yolo_txt_name), 'w')
+# generate training dataframe
+train = pd.read_csv('./train.csv')
+print(train.head())
 
-	for anno in data['annotations']:
-		if anno['image_id'] == img_id:
-			yolo_bbox = cc2yolo_bbox(img_width, img_height, anno['bbox'])  # "bbox": [x,y,width,height]
-			yolo_txt_file.write(
-				'{} {} {} {} {}\n'.format(cate_id_map[anno['category_id']], yolo_bbox[0], yolo_bbox[1], yolo_bbox[2],
-										  yolo_bbox[3]))
-	yolo_txt_file.close()
-f.close()
+train_df, valid_df = train_test_split(train, test_size=0.1, random_state=233)
+print(f'Size of total training images: {len(train)}, '
+	  f'training images:{len(train_df)} . validation images: {len(valid_df)}')
+
+# generate newtrain data frame with spliter mark
+train_df.loc[:, 'split'] = 'train'
+valid_df.loc[:, 'split'] = 'valid'
+df = pd.concat([train_df, valid_df]).reset_index(drop=True)
+print(df.sample(10))
+# mdke directory for traning secti
+# cd /kaggle/training/
+# ls
+os.makedirs('./training/cowboy/images/train', exist_ok=True)
+os.makedirs('./training/cowboy/images/valid', exist_ok=True)
+
+os.makedirs('./training/cowboy/labels/train', exist_ok=True)
+os.makedirs('./training/cowboy/labels/valid', exist_ok=True)
+
+# move the images and annotations to relevant splited folders
+
+# for i in tqdm(range(len(df))):
+# 	row=df.loc[i]
+# 	name=row.file_name.split('.')[0]
+# 	if row.split=='train':
+# 		copyfile(f'./cowboyoutfits/images/{name}.jpg',
+# 				 f'./training/cowboy/images/train/{name}.jpg')
+# 		copyfile(f'./training/yolo_anno/{name}.txt',
+# 				 f'./training/cowboy/images/train/{name}.txt')
+# 	else:
+# 		copyfile(f'./cowboyoutfits/images/{name}.jpg',
+# 				 f'./training/cowboy/images/train/{name}.jpg')
+# 		copyfile(f'./training/yolo_anno/{name}.txt',
+# 				 f'./training/cowboy/labels/valid/{name}.txt')
+
+# Create yaml file
+# cd ./training
+data_yaml = dict(
+	train='../cowboy/images/train/',
+	val='../cowboy/images/valid',
+	nc=5,
+	names=['belt', 'sunglasses', 'boot', 'cowboy_hat', 'jacket']
+)
+# we will make the file under the yolov5/data/ directory.
+with open('./training/yolov5/data/data.yaml', 'w') as outfile:
+	yaml.dump(data_yaml, outfile, default_flow_style=True)
+
+lr0: 0.01  # initial learning rate (SGD=1E-2, Adam=1E-3)
+lrf: 0.2  # final OneCycleLR learning rate (lr0 * lrf)
+momentum: 0.937  # SGD momentum/Adam beta1
+weight_decay: 0.0005  # optimizer weight decay 5e-4
+warmup_epochs: 3.0  # warmup epochs (fractions ok)
+warmup_momentum: 0.8  # warmup initial momentum
+warmup_bias_lr: 0.1  # warmup initial bias lr
+box: 0.05  # box loss gain
+cls: 0.5  # cls loss gain
+cls_pw: 1.0  # cls BCELoss positive_weight
+obj: 1.0  # obj loss gain (scale with pixels)
+obj_pw: 1.0  # obj BCELoss positive_weight
+iou_t: 0.20  # IoU training threshold
+anchor_t: 4.0  # anchor-multiple threshold
+# anchors: 3  # anchors per output layer (0 to ignore)
+fl_gamma: 0.0  # focal loss gamma (efficientDet default gamma=1.5)
+hsv_h: 0.015  # image HSV-Hue augmentation (fraction)
+hsv_s: 0.7  # image HSV-Saturation augmentation (fraction)
+hsv_v: 0.4  # image HSV-Value augmentation (fraction)
+degrees: 0.0  # image rotation (+/- deg)
+translate: 0.1  # image translation (+/- fraction)
+scale: 0.5  # image scale (+/- gain)
+shear: 0.0  # image shear (+/- deg)
+perspective: 0.0  # image perspective (+/- fraction), range 0-0.001
+flipud: 0.0  # image flip up-down (probability)
+fliplr: 0.5  # image flip left-right (probability)
+mosaic: 1.0  # image mosaic (probability)
+mixup: 0.0  # image mixup (probability)
+copy_paste: 0.0  # segment copy-paste (probability)
+
+# IMG_SIZE = 640  # the default image size in yolo is 640, it will automated resize our image during training and valudation.
+BATCH_SIZE = 32  # wisely choose, use the largest size that can feed up all your gpu ram
+EPOCHS = 5
+MODEL = 'yolov5m.pt'  # 5s, 5m 5l
+name = f'{MODEL}_BS_{BATCH_SIZE}_EP_{EPOCHS}'
+
+'''
+python train.py --batch 32 \
+                 --epochs 5 \
+                 --data data.yaml \
+                 --weights  yolov5m.pt \
+                 --save_period 1 \
+                 --project ./working/kaggle-cwoboy \
+                 --name yolov5m.pt_BS_32_EP_5 \
+                 --cache-images
+                 '''
