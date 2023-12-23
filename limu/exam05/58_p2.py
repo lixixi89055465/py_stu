ac5f20d1ba331f59827b0ce1b16712f0ef70849e
lixixi89055465 <1850094299@qq.com>
2023-12-23 21:16:30 +0800

    limu

---
+++ limu/exam05/58_p2.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/12/23 16:36
+# @Author  : nanji
+# @Site    : 
+# @File    : 58_p2.py
+# @Software: PyCharm 
+# @Comment : 
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+batch_size, num_steps = 32, 35
+train_iter, vocab = d2l.load_data_time_machine(batch_size, num_steps)
+
+vocab_size, num_hiddens, num_layers = len(vocab), 256, 2
+num_inputs = vocab_size
+device = d2l.try_gpu()
+lstm_layer = nn.LSTM(num_inputs, num_hiddens, num_layers)
+model = d2l.RNNModel(lstm_layer, len(vocab))
+model = model.to(device)
+
+num_epochs, lr = 500, 2
+d2l.train_ch8(model, train_iter, vocab, lr, num_epochs, device)
+
---
+++ limu/exam06/61_p2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/12/23 21:16
+# @Author  : nanji
+# @Site    : 
+# @File    : 61_p2.py
+# @Software: PyCharm 
+# @Comment : 
+import os
+import torch
+from torch import nn
+from d2l import torch as d2l
+
