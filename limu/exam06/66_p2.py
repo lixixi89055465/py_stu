ecaa8024036277c6ef116387e75a098ab06c1239
lixixi89055465 <nanji1850094299@outlook.com>
2023-12-27 22:10:44 +0800

    limu

--- limu/exam06/66_p2.py
+++ limu/exam06/66_p2.py
@@ -34,7 +34,7 @@ class Seq2SeqAttentionDecoder(AttentionDecoder):
 
 	def init_state(self, enc_outputs, enc_valid_lens, *args):
 		outputs, hidden_state = enc_outputs
-		return (outputs.permute(1, 0, 2), hidden_state.enc_valid_len)
+		return (outputs.permute(1, 0, 2), hidden_state, enc_valid_lens)
 
 	def forward(self, X, state):
 		# enc_outputs的形状为(batch_size,num_steps,num_hiddens).
@@ -43,13 +43,28 @@ class Seq2SeqAttentionDecoder(AttentionDecoder):
 		X = self.embedding(X).permute(1, 0, 2)
 		outputs, self._attention_weights = [], []
 		for x in X:
-			query=torch.unsqueeze(hidden_state[-1],dim=1)
-			context=self.attention(
-				query,enc_outputs,enc_outputs,enc_valid_lens )
-			x=torch.cat((context,torch.unsqueeze(x,dim=1)),dim=-1)
-			out,hidden_state=self.rnn(x.permute(1,0,2),hidden_state)
+			query = torch.unsqueeze(hidden_state[-1], dim=1)
+			context = self.attention(
+				query, enc_outputs, enc_outputs, enc_valid_lens)
+			x = torch.cat((context, torch.unsqueeze(x, dim=1)), dim=-1)
+			out, hidden_state = self.rnn(x.permute(1, 0, 2), hidden_state)
 			outputs.append(out)
-			self._attention_weights.append(self.attention_weights)
-		outputs=self.dense(torch.cat(outputs,dim=0))
-		return outputs.permute(1,0,2),[enc_outputs,hidden_state,enc_valid_lens]
+			self._attention_weights.append(self.attention.attention_weights)
+		outputs = self.dense(torch.cat(outputs, dim=0))
+		return outputs.permute(1, 0, 2), [enc_outputs, hidden_state, enc_valid_lens]
 
+	@property
+	def attention_weights(self):
+		return self._attention_weights
+
+
+encoder = d2l.Seq2SeqEncoder(vocab_size=10, embed_size=8, num_hiddens=16, \
+							 num_layers=2)
+encoder.eval()
+decoder = Seq2SeqAttentionDecoder(vocab_size=10, embed_size=8, num_hiddens=16, \
+								  num_layer=2)
+decoder.eval()
+X = torch.zeros((4, 7), dtype=torch.long)
+state = decoder.init_state(encoder(X), None)
+output, state = decoder(X, state)
+print(output.shape, len(state), state[0].shape, len(state[1]), state[1][0].shape)
