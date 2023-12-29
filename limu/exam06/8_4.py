23a4515c52391328e22e025e08601402b69b184d
xiang.li4 <wb_xiang.li4@sinitek.com>
2023-12-29 17:07:23 +0800

    limu

--- limu/exam06/8_4.py
+++ limu/exam06/8_4.py
@@ -9,3 +9,10 @@ import os
 import torch
 from torch import nn
 from d2l import torch as d2l
+
+X, W_xh = torch.normal(0, 1, (3, 1)), torch.normal(0, 1, (1, 4))
+H, W_hh = torch.normal(0, 1, (3, 4)), torch.normal(0, 1, (4, 4))
+
+# 3,4 + 3,4
+a = torch.matmul(X, W_xh) + torch.matmul(H, W_hh)
+print(a)
---
+++ limu/exam06/8_5.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time : 2023/12/29 17:05
+# @Author : nanji
+# @Site : 
+# @File : 8_5.py
+# @Software: PyCharm 
+# @Comment : 
+import os
+import torch
+from torch import nn
+from d2l import torch as d2l
