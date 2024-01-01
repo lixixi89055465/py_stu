635cb96970bc4352dfbb9a4d31d5374f376b69db
lixixi89055465 <nanji1850094299@outlook.com>
2024-01-02 00:32:24 +0800

    limu

---
+++ limu/exam07/70_p3.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time : 2024/1/2 0:12
+# @Author : nanji
+# @Site : 
+# @File : 70_p3.py
+# @Software: PyCharm 
+# @Comment : 
+import os
+import torch
+from torch import nn
+from d2l import torch as d2l
+
