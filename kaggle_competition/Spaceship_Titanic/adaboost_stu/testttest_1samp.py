07bfe4e40af2331a47a6b0b66519a44f9881120a
lixixi89055465 <nanji1850094299@outlook.com>
2024-01-13 09:53:56 +0800

    limu

--- kaggle_competition/Spaceship_Titanic/adaboost_stu/testttest_1samp.py
+++ kaggle_competition/Spaceship_Titanic/adaboost_stu/testttest_1samp.py
@@ -6,11 +6,12 @@
 # @Software: PyCharm 
 # @Comment :
 import numpy as np
-
-a= np.full(shape=(30, 355))
-b=np.random.normal(0,3,20)
-# factory_a = np.full(shape=(30, 355)) + np.random.normal(0, 3, 20)
+from scipy  import stats
+a = np.full(shape=(30, 355), fill_value=3)
+b = np.random.normal(0, 3, 30)
 print(a.shape)
 print(b.shape)
-# factory_b = np.full(shape=(30, 355)) + np.random.normal(0, 3, 20)
+factory_a = np.full(shape=(30, 355)) + np.random.normal(0, 3, 30)
+factory_b = np.full(shape=(30, 355)) + np.random.normal(0, 3, 30)
+# a_stat,a_pval=stats.ttest_1samp(a=factory_a,popmean=)
 
