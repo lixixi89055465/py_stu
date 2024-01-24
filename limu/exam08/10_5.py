94fade6cb7c1d51d4b8caa5080e86b1f3c0aaece
xiang.li4 <wb_xiang.li4@sinitek.com>
2024-01-24 13:11:38 +0800

    limu

---
+++ limu/exam08/10_5.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time : 2024/1/24 11:08
+# @Author : nanji
+# @Site : 
+# @File : 10_5.py
+# @Software: PyCharm 
+# @Comment : 
+import os
+import torch
+from torch import nn
+from d2l import torch as d2l
