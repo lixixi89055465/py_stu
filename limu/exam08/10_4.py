53d53051a1bad2ac7717edb065de94ced993a6da
xiang.li4 <wb_xiang.li4@sinitek.com>
2024-01-24 11:06:06 +0800

    limu

--- limu/exam08/10_4.py
+++ limu/exam08/10_4.py
@@ -33,7 +33,7 @@ class Seq2SeqAttentionDecoder(AttentionDecoder):
 		self.rnn = nn.GRU(embed_size + num_hiddens, \
 						  num_hiddens, \
 						  num_layers, \
-						  dropout)
+						  dropout=dropout)
 		self.dense = nn.Linear(num_hiddens, vocab_size)
 
 	def init_state(self, enc_outputs, enc_valid_lens, *args):
@@ -49,6 +49,7 @@ class Seq2SeqAttentionDecoder(AttentionDecoder):
 			context = self.attention(
 				query, enc_outputs, enc_outputs, enc_valid_lens
 			)
+
 			x = torch.cat((context, torch.unsqueeze(x, dim=1)), dim=-1)
 			out, hidden_state = self.rnn(x.permute(1, 0, 2), hidden_state)
 			outputs.append(out)
@@ -68,8 +69,43 @@ encoder.eval()
 decoder = Seq2SeqAttentionDecoder(vocab_size=10, embed_size=8, num_hiddens=16, \
 								  num_layers=2)
 decoder.eval()
-X = torch.zeros((4, 7), dtype=torch.long)
-state = decoder.init_state(encoder(X), None)
-output, state = decoder(X, state)
-print('5' * 100)
-print(output.shape, len(state), state[0].shape, len(state[1]), state[1][0].shape)
+# X = torch.zeros((4, 7), dtype=torch.long)
+# state = decoder.init_state(encoder(X), None)
+# output, state = decoder(X, state)
+# print('5' * 100)
+# print(output.shape, len(state), state[0].shape, len(state[1]), state[1][0].shape)
+
+embed_size, num_hiddens, num_layers, dropout = 32, 32, 2, 0.1
+batch_size, num_steps = 64, 10
+lr, num_epochs, device = 0.005, 250, d2l.try_gpu()
+train_iter, src_vocab, tgt_vocab = d2l.load_data_nmt(
+	batch_size, num_steps,
+)
+encoder = d2l.Seq2SeqEncoder(
+	len(src_vocab), embed_size, num_hiddens, num_layers, dropout
+)
+decoder = Seq2SeqAttentionDecoder(
+	len(tgt_vocab), embed_size, num_hiddens, num_layers, dropout
+)
+net = d2l.EncoderDecoder(encoder, decoder)
+d2l.train_seq2seq(net, train_iter, lr, num_epochs, tgt_vocab, device)
+
+
+print('0'*100)
+engs = ['go .', "i lost .", 'he\'s calm .', 'i\'m home .']
+fras = ['va !', 'j\'ai perdu .', 'il est calme .', 'je suis chez moi .']
+for eng, fra in zip(engs, fras):
+    translation, dec_attention_weight_seq = d2l.predict_seq2seq(
+        net, eng, src_vocab, tgt_vocab, num_steps, device, True)
+    print(f'{eng} => {translation}, ',
+          f'bleu {d2l.bleu(translation, fra, k=2):.3f}')
+
+
+attention_weights = torch.cat([step[0][0][0] for step in dec_attention_weight_seq], 0).reshape((
+    1, 1, -1, num_steps))
+
+
+# 加上一个包含序列结束词元
+d2l.show_heatmaps(
+    attention_weights[:, :, :, :len(engs[-1].split()) + 1].cpu(),
+    xlabel='Key positions', ylabel='Query positions')
