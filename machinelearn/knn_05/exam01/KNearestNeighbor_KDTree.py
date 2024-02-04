8c928029eddedffc3646b9387dab9e8909c11df5
xiang.li4 <wb_xiang.li4@sinitek.com>
2024-02-04 18:00:23 +0800

    machine learning

--- machinelearn/knn_05/exam01/KNearestNeighbor_KDTree.py
+++ machinelearn/knn_05/exam01/KNearestNeighbor_KDTree.py
@@ -8,10 +8,11 @@
 import numpy as np
 import heapq
 from machinelearn.knn_05.exam01.DistanceUtils import DistanceUtils
+from collections import Counter
 from machinelearn.knn_05.exam01.KDTreeNode import KDTreeNode
 
 
-class KNearestNeighbor_KDTree:
+class KNearestNeighborKDTree1:
 	def __init__(self, k=3, p=2, view_kdt=False):
 		self.k = k
 		self.p = p
@@ -26,6 +27,7 @@ class KNearestNeighbor_KDTree:
 		self.k_dimension = x_train.shape[1]
 		sample_idx_array = np.arange(x_train.shape[0])
 		self.kdt_root = self._build_kd_tree(x_train, y_train, sample_idx_array, 0)
+		print('2'*100)
 
 	# if self.view_kdt:
 	# 	self.draw_kd_tree()
@@ -58,28 +60,50 @@ class KNearestNeighbor_KDTree:
 								  idx_array[median_idx],
 								  split_dimension,
 								  left_child, right_child, depth)
+		return kdt_new_node
 
 	def search_kd_tree(self, kd_tree, x_test):
 		if kd_tree is None:
 			return
 		else:
-			distance = self.distance_utils.distance_func(kd_tree.instancec_node, x_test)
+			distance = self.distance_utils.distance_func(kd_tree.instance_node, x_test)
 			self.k_neighbor.reverse()
 			if (len(self.k_neighbor) < self.k) or (distance < self.k_neighbor[0]['distance']):
 				self.search_kd_tree(kd_tree.left_child, x_test)
-				self.sarch_kd_tree(kd_tree.right_child, x_test)
+				self.search_kd_tree(kd_tree.right_child, x_test)
 				self.k_neighbor.append({
-					'node': kd_tree.instancec_node,
+					'node': kd_tree.instance_node,
 					'label': kd_tree.instance_label,
 					'distance': distance
 				})
 				self.k_neighbor = heapq.nsmallest(self.k, self.k_neighbor, \
 												  key=lambda d: d['distance'])
 
-	def predict(self,x_test):
+	def predict(self, x_test):
 		'''
 		对测试样本进行类别预测
 		:param x_test: 测试样本
 		:return:
 		'''
-		x_test=np.asarray(x_test)
+		x_test = np.asarray(x_test)
+		if self.k < 1:
+			raise ValueError('k must be greater than 0.')
+		elif self.kdt_root is None:
+			raise ValueError('KDTree is None.')
+		elif x_test.shape[1] != self.k_dimension:
+			raise ValueError("target node's dimension unmatched KDTree's dimension ")
+		else:
+			y_test_hat = []
+			for i in range(x_test.shape[0]):
+				y_test_labels = []
+				self.k_neighbor = []
+				self.search_kd_tree(self.kdt_root, x_test[i])
+				for k in range(self.k):
+					y_test_labels.append(self.k_neighbor[k]['label'])
+				count, best_label, freq = 0, None, Counter(y_test_labels)
+				for label in freq.keys():
+					if freq[label] > count:
+						best_label, count = label, freq[label]
+				y_test_hat.append(best_label)
+			return np.array(y_test_hat)
+
