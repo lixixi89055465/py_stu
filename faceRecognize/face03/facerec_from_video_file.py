81a5c536019c7c2310c940ccc4052a281fa214ea
lixixi89055465 <nanji1850094299@outlook.com>
2024-09-30 00:10:35 +0800

    自动上妆

--- faceRecognize/face03/facerec_from_video_file.py
+++ faceRecognize/face03/facerec_from_video_file.py
@@ -23,7 +23,7 @@ fourcc = cv2.VideoWriter.fourcc(*'XVID')
 # 常用的有"DIVX","MJPG","XVID","X264"。可用的列表在这里 .
 # 推荐使用"XVID",但一般依据你的电脑环境安装了哪些编码器 .
 output_movie = cv2.VideoWriter('output.avi', fourcc, 25, (640, 360))
-output_movie = cv2.VideoWriter('output.avi', fourcc, 29.97, (640, 360))
+# output_movie = cv2.VideoWriter('output.avi', fourcc, 29.97, (640, 360))
 # load some sample pictures and learn how to recognize them.
 lmm_image = face_recognition.load_image_file('Qidian.png')
 lmm_face_encoding = face_recognition.face_encodings(lmm_image)[0]
@@ -32,5 +32,39 @@ al_image = face_recognition.load_image_file('Quxiaoxiao.png')
 al_face_encoding = face_recognition.face_encodings(al_image)[0]
 
 known_faces = [
-
+	lmm_face_encoding,
+	al_face_encoding
 ]
+face_locations = []
+face_encodings = []
+face_names = []
+frame_number = 0
+
+while True:
+	ret, frame = input_movie.read()
+	frame_number += 1
+	if not ret:
+		break
+	rgb_frame = frame[:, :, ::-1]
+	face_locations = face_recognition.face_locations(rgb_frame)
+	face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+	face_names = []
+	for face_encoding in face_encodings:
+		match = face_recognition.compare_faces(known_faces, face_encoding, tolerance=0.5)
+		name = None
+		if match[0]:
+			name = 'Qidian'
+		elif match[1]:
+			name = 'Quxiaoxiao'
+		face_names.append(name)
+	for (top, right, bottom, left), name in zip(face_locations, face_names):
+		if not name:
+			continue
+		cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+		cv2.rectangle(frame, (left, bottom - 25), (right, bottom), (0, 0, 255), cv2.FILLED)
+		font = cv2.FONT_HERSHEY_DUPLEX
+		cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.5, (255, 255, 255), 1)
+	print('writing frame {} / {} '.format(frame_number, length))
+	output_movie.write(frame)
+input_movie.release()
+cv2.destroyAllWindows()
