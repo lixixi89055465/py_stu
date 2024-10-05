950bc57a4c35bcfa24a6c0f49b6d51886fbe56c0
lixixi89055465 <nanji1850094299@outlook.com>
2024-10-05 23:16:19 +0800

    大数据LLama

--- bigModel/wuenda_llm/Lab5_Training.py
+++ bigModel/wuenda_llm/Lab5_Training.py
@@ -5,4 +5,29 @@
 # @File : Lab5_Training.py
 # @Software: PyCharm 
 # @Comment :
+import datasets
+import tempfile
+import logging
+import random
+import config
+import os
+import yaml
+import logging
+import time
+import torch
+import transformers
 
+from bigModel.wuenda_llm.utilities import *
+from transformers import AutoTokenizer
+from transformers import AutoModelForCausalLM
+from transformers import TrainingArguments
+from transformers import AutoModelForCausalLM
+
+logging = logging.getLogger(__name__)
+global_config = None
+dataset_name = 'lamini_docs.jsonl'
+dataset_path = f"/content/{dataset_name}"
+use_hf = False
+
+dataset_path = 'lamini/lamini_docs'
+use_hf = True
